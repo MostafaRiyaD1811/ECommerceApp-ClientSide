afdd23353cac19dce276e5cc52eb4af986c4ce99
Alaa AboElhassan <alaa.aboelhassan@outlook.com>
2022-07-31 23:36:50 +0200

    edit nav -bar

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-<<<<<<< HEAD
 import { AccountService } from 'src/app/account/account.service';
 import { IBasket } from 'src/app/shared/models/basket';
 import { IUser } from 'src/app/shared/models/user';
-=======
 import { BasketService } from 'src/app/basket/basket.service';
-import { IBasket } from 'src/app/shared/models/basket';
->>>>>>> 2876b535df5dd24b11b16e11746efb422b573a9b
 import {ShopComponent} from 'src/app/shop/shop.component'
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -17,23 +14,19 @@ import {ShopComponent} from 'src/app/shop/shop.component'
 })
 export class NavBarComponent implements OnInit {
   currentUser$: Observable<IUser>;
-  constructor(private accountService: AccountService) {
-
   basket$:Observable<IBasket>;
-  constructor(private basketService:BasketService) { }
+  constructor(private accountService: AccountService, private basketService: BasketService)
+  {
 
+  }
   ngOnInit(): void {
     this.basket$=this.basketService.basket$;
-  }
-
-  ngOnInit(): void {
-   this.currentUser$ = this.accountService.CurrentUser$;
+    this.currentUser$ = this.accountService.CurrentUser$;
   }
 
   logOut() {
     this.accountService.logout();
   }
-}  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+}
+  
 
